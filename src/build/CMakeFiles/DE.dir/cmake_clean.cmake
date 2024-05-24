3ec0db55d137a8eac5d9acfb9c987bf4aa727032
Hsiao yen Liang <hsiaoyenlaing@ViviShare.local>
2024-05-24 15:54:34 +0800

    add Cmake

---
+++ src/build/CMakeFiles/DE.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DE.dir/main.cpp.o"
+  "CMakeFiles/DE.dir/main.cpp.o.d"
+  "DE"
+  "DE.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
